92968dd53519252624263e2b173d97125237e92e
chatomzcoding <coding.chatomz@gmail.com>
2022-02-22 15:01:15 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $menu   = 'dashboard';
+        return view('admin.dashboard', compact('menu'));
+    }
+}
